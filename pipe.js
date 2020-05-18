d010202a2ae56339e634156a3bbfab72e5235539
Anay2805 <62181324+Anay2805@users.noreply.github.com>
2020-05-18 12:11:25 +0530

    Add files via upload

---
+++ pipe.js
@@ -0,0 +1,36 @@
+
+function Pipe() {
+  this.spacing = 120;
+  this.top = random(height / 6, (3 / 4) * height) ;
+  this.bottom = height - (this.top + this.spacing);
+  this.x = width;
+  this.w = 110;
+  this.speed = 2;
+
+  this.hits = function(bird) {
+    if (bird.y < this.top- 10|| bird.y > height - this.bottom - 40) {
+      if (bird.x > this.x - 70  && bird.x < this.x + this.w) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  this.show = function() {
+    fill(0, 200, 61);
+    rect(this.x, 0, this.w, this.top);
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  };
+
+  this.update = function() {
+    this.x -= this.speed;
+  };
+ 
+  this.offscreen = function() {
+    if (this.x < -this.w) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+}
